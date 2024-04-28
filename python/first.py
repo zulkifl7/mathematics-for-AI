5048f799de478439e91311872d99cfcce722f140
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-04-28 16:25:10 +0530

    Added python files

---
+++ python/first.py
@@ -0,0 +1,21 @@
+# input
+# process 
+
+
+
+# output
+
+name = input("Enter your name: ")
+age = input("Enter Your age: ")
+
+
+print(f"My name is {name} and i am {age} years old!")
+
+# print(f"my name is {name}. I am {age} years old.")
+
+
+
+
+
+
+
